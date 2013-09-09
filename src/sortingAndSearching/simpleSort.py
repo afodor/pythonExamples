0784d393baba591ec5dbf723909309c7a822b1a4
afodor <anthony.fodor@gmail.com>
2013-09-09 12:25:18 -0400

    Adding simple bubble sort

---
+++ src/sortingAndSearching/simpleSort.py
@@ -0,0 +1,20 @@
+
+# a slow n^2 naive sorting algorithm
+def bubbleSort( aList ):
+	for i in range(1 , len(aList) -1 ):
+		for j in range( i , len(aList ) ):
+			if( aList[i] > aList[j] ):
+				temp = aList[i]
+				aList[i] = aList[j]
+				aList[j] = temp
+
+myList = [1,2,31,2,1,2,3,4,5,0,-1,3,4]
+myList
+bubbleSort(myList)
+myList
+
+myList = ["A", "C", "Z", "E", "Y", "Roger"]
+myList
+bubbleSort(myList)
+myList
+				
