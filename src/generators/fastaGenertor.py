9a1314f488f69d57553347aa50ac5244d443d6e0
afodor <anthony.fodor@gmail.com>
2013-09-11 15:57:18 -0400

    adding sorting

--- src/generators/fastaGenertor.py
+++ src/generators/fastaGenertor.py
@@ -11,6 +11,19 @@ def fastaAsGenerator(filepath):
 		yield ( header.replace("\n",""), seq.replace("\n","" ) ) 
 		header = aLine		
 	f.close()
+	
+def gcContent(fastaSeq):
+	return fastaSeq.count("C") + fastaSeq.count("G")
 
 myGen = fastaAsGenerator("C:\\classes\\undergradProgramming_2013\\sequences\\test.fasta")
+
+aList = []
+
+for seq in myGen:
+	aList.append(seq)
+
+sorted( aList, key=gcContent) 
+
+sorted(myGen)
+
 next(myGen)
