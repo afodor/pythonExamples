6771c69ab49b3cbd5178c65cd962608cf9514358
afodor <anthony.fodor@gmail.com>
2013-10-05 22:28:25 -0400

    adding a circle class

---
+++ src/ooExamples/Circle.py
@@ -0,0 +1,14 @@
+
+class Circle:	
+	
+	def __init__(self,radius):
+		self.radius = radius
+	
+	def setRadius(self, radius):
+		self.radius = radius
+	
+	def getRadius():
+		return self.radius;
+	
+	def getArea():
+		return math.pi * self.radius ** 2
