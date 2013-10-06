27fef718738c4e18e27c972152a4030de904c984
afodor <anthony.fodor@gmail.com>
2013-10-05 21:43:45 -0400

    making an OO FastaParser

--- src/ooExamples/ooExamples.py
+++ src/ooExamples/ooExamples.py
@@ -1,17 +1,48 @@
 
-class Foo:
+
+
+class FastaSequence:
 	
-	a=5
-	
-	def getB(self):
-		self.b=10
-		return self.b
+	def __init__( self, header, sequence):
+		self.header = header
+		self.sequence = sequence
+		
+	def getHeader(self):
+		return self.header
+		
+	def getSequence(self):
+		return self.sequence
 		
 
+#returns a tuple with (header,sequence) 
+def fastaAsGen(filepath):
+	f=open(filepath)
+	header=f.readline()
+	line=f.readline()
+	seq=""
+	while( len(line) > 0 ):
+		seq = seq + line
+		line = f.readline()
+		if line.startswith(">") or len(line)==0:
+			yield ( FastaSequence( header.replace("\n",""), seq.replace("\n","")) )
+			header = line
+			seq = ""
+			line = f.readline() 
+	f.close()		
+
 	
+myGen = fastaAsGen("D:\\classes\\undergradProgramming_2013\\sequences\\NC-016613.faa")
 
-		
+mySeq = next(myGen)
 
+mySeq
+
+mySeq.getHeader()
+mySeq.getSequence()
+
+# considered poor form but allowed in Python
+mySeq.header
+mySeq.sequence
 
 
 
