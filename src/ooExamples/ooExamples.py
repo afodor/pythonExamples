145850aa97c586508ba0bd3fbdffb5a5c2b7f0e7
afodor <anthony.fodor@gmail.com>
2013-10-05 21:34:31 -0400

    stub functions

---
+++ src/ooExamples/ooExamples.py
@@ -0,0 +1,25 @@
+
+class Foo:
+	
+	a=5
+	
+	def getB(self):
+		self.b=10
+		return self.b
+		
+
+	
+
+		
+
+
+
+
+
+
+
+
+
+		
+	
+
---
+++ src/sortingAndSearching/LinearSearch.py
@@ -0,0 +1,5 @@
+
+def find_A_3( myIterable ):
+	for i in range(0,len(myIterable)):
+		if( myIterable[i] == 3): 
+			return i
