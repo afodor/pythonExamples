059351c5c7ffe8079cad8a5f2d9212df9a373e0a
afodor <anthony.fodor@gmail.com>
2013-11-02 11:08:39 -0400

    towards Viterbi

--- src/viterbi/viterbiExample.py
+++ src/viterbi/viterbiExample.py
@@ -17,6 +17,12 @@ class MarkovState:
 			index = index + 1
 		return len(self.emissionProbs) - 1
 		
+	def getIndexOfEmission(self, char):
+		for i in range(0, len(charsToEmit) ):
+			if charsToEmit[i] == char:
+				return i
+		raise Exception("Cound not find " + char )
+				
 	def getTransitionIndex(self):
 		aRand = random.random()
 		cumulative = 0
@@ -28,6 +34,35 @@ class MarkovState:
 			index = index + 1
 		return len(self.transitionProbs) - 1
 
+def getMaxIndex( iterable ):
+	val = iterable[0]
+	index =0
+	returnVal =0
+	for i in iterable:
+		if i < val:
+			returnVal = index
+		index = index+1
+	return returnVal
+	
+
+def getViterbiPath( markovStates, output, emissions ):
+	viterbiPath = []
+	oldViterbiProbs = []
+	oldViterbiProbs[0] = 1 # we are 100% sure we start in the first state
+	for i in range( 1, len(markovStates) ):
+		oldViterbiProbs[i] = 0
+	
+	for i in range( 0,emissions):
+		newViterbiProbs = []
+		for j in range( 0, len(markovStates)):
+			emissionProb = state.emissionProbs[state.getIndexOfEmission(j)]		
+			vTimesA=[]
+			for k in range(0, markovStates):
+				vTimesA[k] = oldViterbiProbs[j] * markovStates[j].transitionProbs[k]
+			maxVal = vTimesA[ getMaxIndex(vTimesA) ]
+			newViterbiProbs[j] = emissionProb * maxVal
+		viterbiPath[i] = getMaxIndex(newViterbiProbs)	
+	return viterbiPath
 
 dice = ( 1,2,3,4,5,6 ) 
 
@@ -40,7 +75,7 @@ rolls = ""
 trueStates = ""
 state = states[0]
 
-for i in range( 1, 1000):
+for i in range( 1, 100):
 	nextState = state.getTransitionIndex()
 	state = states[ nextState]
 	trueStates = trueStates + str(nextState)
