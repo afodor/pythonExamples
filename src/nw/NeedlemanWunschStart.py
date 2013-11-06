b66ab61fe9dfd72b09ee82577b3616e28e963095
afodor <anthony.fodor@gmail.com>
2013-11-06 17:03:06 -0500

    Expanding starter code for NW homework

--- src/nw/NeedlemanWunschStart.py
+++ src/nw/NeedlemanWunschStart.py
@@ -1,3 +1,4 @@
+import sys
 
 class Cell:
 	def __init__( self, direction, score):
@@ -21,3 +22,14 @@ myMatrix = [[Cell("init",0) for x in range(0,len2)] for x in range(0,len1)]
 for x in range(0, len1):
 	for y in range(0,len2):
 		print( str(x) + "," + str(y) + ":" +  str( myMatrix[x][y].getScore() ) + " " + str( myMatrix[x][y].getDirection() ) ) 
+		
+
+myMatrix[0][1] = Cell("left", -8)
+myMatrix[1][0] = Cell("up", -8)
+
+# print it out in a more useful grid
+for x in range(0, len1):
+	aString = ""
+	for y in range(0,len2):
+		aString = aString + ( str( myMatrix[x][y].getScore() ) + "_" + str( myMatrix[x][y].getDirection() ) + str("\t") ) 
+	print(aString + "\n")
